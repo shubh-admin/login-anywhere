5f502df68aa2ef69cfa5e71f8c39b81065407b8a
Gaurav Arora <gaurav.arora6@gmail.com>
2015-08-30 01:21:55 +0530

    Create login_form_callback.php

---
+++ shortcodes/login_form_callback.php
@@ -0,0 +1,27 @@
+<?php
+if(!is_user_logged_in())
+{
+$args = array(
+        'echo'           => true,
+        'redirect' => ( is_ssl() ? 'https://' : 'http://' ) . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'],
+        'form_id'        => 'loginform',
+        'label_username' => __( 'Username' ),
+        'label_password' => __( 'Password' ),
+        'label_remember' => __( 'Remember Me' ),
+        'label_log_in'   => __( 'Log In' ),
+        'id_username'    => 'user_login',
+        'id_password'    => 'user_pass',
+        'id_remember'    => 'rememberme',
+        'id_submit'      => 'wp-submit',
+        'remember'       => true,
+        'value_username' => '',
+        'value_remember' => false
+);
+
+ wp_login_form( $args );
+}
+else
+{
+	echo "You are already logged in";
+}
+?>
